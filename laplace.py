d04daa373eeab54327d6614f88f8642a1dd78c73
Andrew Cook <30913007+ajcook14@users.noreply.github.com>
2026-07-23 17:55:29 +1000

    plot solution

--- laplace.py
+++ laplace.py
@@ -4,6 +4,10 @@ import torch as pt
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
+import numpy as np
+from matplotlib.ticker import LinearLocator
+
+
 class NET(pt.nn.Module):
     def __init__(self):
         super().__init__()
@@ -41,3 +45,27 @@ for i in range(10):
     loss.backward() # be aware data.grad changes via the .backward() call
 
     optimizer.step() # only alters the model parameter values
+
+with pt.no_grad():
+    U = model(data).numpy()
+
+U = U.reshape((N_POINTS, N_POINTS))
+X, Y = np.meshgrid(x.numpy(), y.numpy())
+
+ax = plt.figure().add_subplot(projection='3d')
+
+colortuple = ('y', 'b')
+colors = np.empty((N_POINTS, N_POINTS), dtype=str)
+
+for y in range(N_POINTS):
+    for x in range(N_POINTS):
+        colors[y, x] = colortuple[(x + y) % len(colortuple)]
+
+# Plot the surface with face colors taken from the array we made.
+surf = ax.plot_surface(X, Y, U, facecolors=colors, linewidth=0)
+
+# Customize the z axis.
+ax.set_zlim(0, 1)
+ax.zaxis.set_major_locator(LinearLocator(6))
+
+plt.show()
